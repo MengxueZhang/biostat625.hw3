5628bdc806588824eb866d342a950d8833e6814b
Mengxue Zhang <mengxuez@umich.edu>
2023-11-10 17:34:29 -0500

    Added help page using Roxygen2

--- R/SLR_coeff.R
+++ R/SLR_coeff.R
@@ -1,13 +1,26 @@
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
+#' simple_linear_regression_coeffs
+#'
+#' Calculate the estimated covariate' coefficients for simple linear regression
+#'
+#' @param user_dat user data
+#' @param x choice of covariate
+#' @param y choice of response variable
+#'
+#' @return The estimated intercept and slope of the simple linear regression model
+#'
+#' @examples
+#' df1 <- data.frame(x = c(1, 2, 3, 4, 5), y = c(1, 5, 8, 15, 26))
+#' simple_linear_regression_coeffs(df1, x = df1$x, y = df1$y)
+#'
+#' ctl <- c(4.17, 5.58, 5.18, 6.11, 4.50, 4.61, 5.17, 4.53, 5.33, 5.14)
+#' trt <- c(4.81, 4.17, 4.41, 3.59, 5.87, 3.83, 6.03, 4.89, 4.32, 4.69)
+#' group <- gl(2, 10, 20, labels = c("0", "1"))
+#' weight <- c(ctl, trt)
+#' df2 <- data.frame(weight, group)
+#' simple_linear_regression_coeffs(df2, x = as.numeric(group), y = weight)
+#'
+#' @export
+#'
 
 simple_linear_regression_coeffs <- function(user_dat, x, y) {
 
